f101ea247783982ee566e03770ea35c12ca97165
Alanvianto <alan.vianto@yahoo.co.id>
2015-03-16 18:32:44 +0700

    Create BonusCounter.java

---
+++ BonusCounter.java
@@ -0,0 +1,14 @@
+public class BonusCounter
+{
+	public static void main (String [] miku)
+	{
+		int jumbrg = 37;
+		int totbns = jumbrg / 2 ;
+		int totbrg = jumbrg + totbns;
+		
+		System.out.println ("------Beli 2 Gratis 1------");
+		System.out.println ("Jumlah Barang\t: "+jumbrg+" barang");
+		System.out.println ("Total Bonus\t: "+totbns+" barang");
+		System.out.println ("Total Barang\t: "+totbrg+" barang");
+	}
+}
